8f0bc514f48584e6e262208794b2817bfe7e9b3a
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-09-13 16:36:27 -0600

    Add topics hook in the Home component:

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Topics from '../Topics/Topics'
+import { getArticles } from '../../Utlities/apiCalls';
 import './Home.css';
 
 const Home = () => {
+  const [topics, setTopics] = useState(['arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'home', 'insider', 'magazine', 'movies', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 't-magazine', 'travel', 'upshot', 'us', 'world'])
+
+
   return (
     <div className="App">
       <Topics />
