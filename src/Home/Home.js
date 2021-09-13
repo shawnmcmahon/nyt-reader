f04965e2627312225e9a8f4975af000fc0021562
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-09-13 16:20:17 -0600

    Import Topics component into Home component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Topics from './Topics/Topics'
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Topics />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './src/Home/Home';
+import Home from '../src/Home/Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
